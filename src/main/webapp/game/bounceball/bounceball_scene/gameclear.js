b7228147894a98fa4ad889b88c45ea21e13f5299
MHW <a@b.com>
2023-09-21 10:14:46 +0900

    bounceball_scene contents added

---
+++ src/main/webapp/game/bounceball/bounceball_scene/gameclear.js
@@ -0,0 +1,37 @@
+class GameClearScene extends Phaser.Scene {
+    constructor(){
+        super({key:"GameClearScene"});
+    }
+
+    preload(){
+        this.load.image("background3","/img/bg.jpg");
+        this.load.image("replay","/img/replay.png");
+        this.load.image("gameclear","/img/gameclear.png");
+    }
+
+    create(){
+        let score = document.getElementById("score");
+        this.background = this.add.tileSprite(0,0,1600,1200,"background3");
+        this.background.setOrigin(0,0);
+
+        this.physics.add.sprite(this.cameras.main.width/2,this.cameras.main.height/2-100,"gameclear").setOrigin(0.5).setScale(0.5);
+        this.text = this.add.text(this.cameras.main.width/2,this.cameras.main.height/2,"Record : " + score.value + "s" ,{font:"40px Bahnschrift", fill:"#ffffff"}).setOrigin(0.5);
+        let replyBtn = this.physics.add.sprite(this.cameras.main.width/2,this.cameras.main.height/2+100,"replay").setOrigin(0.5).setScale(0.5).setInteractive();
+
+        replyBtn.on("pointerover",()=>{
+            this.game.canvas.style.cursor = "pointer";
+        });
+
+        replyBtn.on("pointerout",()=>{
+            this.game.canvas.style.cursor = "default";
+        })
+
+        replyBtn.on("pointerdown",()=>{
+            this.scene.start("PlayScene");
+        })
+    }
+
+    update(){
+
+    }
+}
