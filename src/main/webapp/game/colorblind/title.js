dfb5aa6f30589c3b70b420efd8c971cb51cba68b
KMS <msg_1218@naver.com>
2023-09-25 14:19:32 +0900

    Colorblind logo changed

--- src/main/webapp/game/colorblind/title.js
+++ src/main/webapp/game/colorblind/title.js
@@ -9,11 +9,11 @@ class Title extends Phaser.Scene {
 
     }
     preload() {
-        this.load.image("logo", "COLORBLIND_remv_bg.png");
+        this.load.image("logo", "Colorblind_Logo_new_removebg.png");
     }
     create() {
 
-        this.cameras.main.setBackgroundColor("#D549F9")
+        this.cameras.main.setBackgroundColor("#FFFFFF");
         
         let logo = this.add.image(this.cameras.main.centerX, this.cameras.main.centerY, "logo");
         logo.setOrigin(0.5, 0.5)
@@ -21,8 +21,8 @@ class Title extends Phaser.Scene {
         let startButton = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY*1.7, 'START GAME')
     .setOrigin(0.5, 0.5)
     .setPadding(10)
-    .setFill("#F7F1FE")
-    .setBackgroundColor("#D549F7")
+    .setFill("#000000")
+    .setBackgroundColor("#FFFFFF")
     .setFontSize("20px")
     .setFontStyle("bold")
     .setInteractive({ useHandCursor: true })
@@ -32,7 +32,7 @@ class Title extends Phaser.Scene {
     this.cameras.main.fadeOut(500, 255,255,255);
     })
     .on('pointerover', () => { startButton.setStyle({ fill: '#f39c12' }) })
-    .on('pointerout', () => { startButton.setStyle({ fill: '#F7F1FE' }) })
+    .on('pointerout', () => { startButton.setStyle({ fill: '#000000' }) })
 
     this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, (cam, effect) => {
         this.scene.transition({target: "ColorBlind"}) 
