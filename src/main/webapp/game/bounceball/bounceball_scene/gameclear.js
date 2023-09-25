cc5101e5c7f932171f69c3b1b467369ddef9dd17
s2zz <hey_spring@naver.com>
2023-09-25 15:51:48 +0900

    bounce ball

--- src/main/webapp/game/bounceball/bounceball_scene/gameclear.js
+++ src/main/webapp/game/bounceball/bounceball_scene/gameclear.js
@@ -10,12 +10,28 @@ class GameClearScene extends Phaser.Scene {
     }
 
     create(){
+$.ajax({
+    url: "/rankReadPoint.rankBoard",
+    data: {
+        score: $("#score").val(),
+        game_name: "bounceball",
+        rank_type: "time"
+    },
+    method: "GET"
+}).done(function(resp) {
+    console.log(resp);
+    if (resp === "true") { // 문자열 "true"와 비교
+        alert("ㅊㅊㅊㅊㅊㅊㅊ");
+    }
+});
+	
+	
         let score = document.getElementById("score");
         this.background = this.add.tileSprite(0,0,800,600,"background3");
         this.background.setOrigin(0,0);
 
         this.physics.add.sprite(this.cameras.main.width/2,this.cameras.main.height/2-100,"gameclear").setOrigin(0.5).setScale(0.5);
-        this.text = this.add.text(this.cameras.main.width/2,this.cameras.main.height/2,"Record : " + score.value + "s" ,{font:"40px Bahnschrift", fill:"#ffffff"}).setOrigin(0.5);
+        this.text = this.add.text(this.cameras.main.width/2,this.cameras.main.height/2,"Record : " + score.value + "s" ,{fill:"#ffffff"}).setOrigin(0.5);
         let replyBtn = this.physics.add.sprite(this.cameras.main.width/2,this.cameras.main.height/2+100,"replay").setOrigin(0.5).setScale(0.5).setInteractive();
 
         replyBtn.on("pointerover",()=>{
